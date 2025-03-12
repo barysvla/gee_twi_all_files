3a795f7d008bdde1cc741e3f1279161b9ee55b19
barysvla <vladislav.baryshev@outlook.com>
2025-03-12 10:57:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,47 @@ from scripts.twi import compute_twi
 from scripts.visualization import visualize_map
 #from scripts.export import export_to_drive, export_to_asset
 
+#-------------------------------------------------
+# 🔹 Uložení service account klíče jako textové proměnné
+service_key_json = """
+{
+  "type": "service_account",
+  "project_id": "gee-project-twi",
+  "private_key_id": "67f3ec4f29eb962ebdbc76c7ebd1a41e5087950a",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDufgcy1IINstaB\nCqbEQkpH48VFyHSh4sGG2S6Lhcss+puK7WH5I+vEoQ3Du2fwzxZ4ZVoqxWLivkCA\nLaPLHjwBt29v5J/Sn8kLVXldGzZM9xITCNnQ4x3+S1OHQXY2J7jlbyCQuSQL912+\n5G3OhB81blOx/bxA/zZel46ROTgviby6aF0+RLz/WYKPlBGTS1CkxAAlE0C2QVZp\ncTCBhA7P5ltDzINz58zOWrBknwUT6G15f/lu2GpaX6gvP0sz7e14ang48WTgdEJ8\nDXgJKUBM+TyYZchDogdR5FSxVHaUoiD/HJtqG+9eEgJdSqqxWVDEfnv0KIsbDFwW\nPDaNHX4tAgMBAAECggEAAotZDGZBHzOuqmvEGr3oZLQI+wzH8htn2WfKGR1Og2D7\nrSGfzf+iaUh7RjLSHQNaqUbEOiUyE4cxYeyw/ELvI6YSFFLzsQakJ3DByNXv9n8F\n2v43vsHBtSjZ3zvV8X7rQKAPyuwp2tnIho3qn4wOFHwm9vQn0VdwB3HhheH5N96B\n85zmLExu432osup41po/nQK4RTo64lR2W8KcCUMVlc/tvsgDaQrOQoldzkPXzVvy\n0odNSchjEPGub9CdXrkQ7So5ACE7IRR+WUzGoYD07zjzc7q+eiq1l9Lc+WvLhO+H\nW/dqRD5h6rzFpqBrxqBofyjodJaDT2W1n+Mh7GOxgQKBgQD7bYYlHD1rgxfwp+UJ\n/zSDoj2PV6agRSgwfNYIPaZ7Rxb7VNfkz4Y2hlTMQpiftD9JnW+//oigvd0l7g8g\ndNQo+i9pjCQqQpcVt8gU8vAHSUHAQ1q24M9+cPSyNG68sS7qun/DulYW3w72ehmk\nDcX/Kql65X7peCWQ+3S/ArH4zQKBgQDy1Ej+ZoPJi6gZ3n/SsuCUcufYF0hB/m5m\nNHZMfzd7O0T9e4hgjVCBnraxsSA1Ze2/+1C/yI95BH4F+yvxvikxym4ZGbunlSKN\nxoyzEzDM0wNx5QaxqdWRU+XvJa4HsJHodrK52emjK4RcY6KC43C+kXgs+PG4Tn1Z\n4+HqLSEa4QKBgQCCcD+34P68WDaDU5cvqIbGA9WMGB0J6NcA0ML5Y4KLUkZJ/apD\nuqYWg3pavfIQaKKsvlDLenHHcrjYhLi0TegDmkgeqeXZRtK7Ia1bsO112juSU18s\noVUEc/V+vlT077c3b6n5ESK4muBYXuAOjFa8GpXyfD1rQjm7DblznyJVwQKBgGKI\nc2oPbRB+Q+LjXxi/DtQN4DfWErL8rA164jneMUQm47K7yrXrAaznGxj1V61zQ8rw\nDm8T0ev4P67roYRBdnPGwXAb+gJhSJkg5PzRH68tPKudlF7JHGkREy88Kebi5gHf\nEFzLqLtWMCpRH6Ne6OPbIVmWVndEGic8ifI9B77hAoGAZfgVBYm5t9X5k6WVQ/Io\nhI/DiKMXdtq9kOkc8Kl98s4U/K/06pI8+iS7eoMNt1DT8l3apLseHBcC1vO0ttwx\nelWlKyZOzJxJxNimUT1KuQXXhZRZ6m7dBdBP/bmjJOvWTEEIwL8lSQo+5pzSJiVe\nduVSmjNiAK5eYunKSnFHuS8=\n-----END PRIVATE KEY-----\n",
+  "client_email": "gee-service-twi@gee-project-twi.iam.gserviceaccount.com",
+  "client_id": "104900013036195137165",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gee-service-twi%40gee-project-twi.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+"""
+
+# 🔹 Převedení textového JSONu na Python objekt
+service_account_info = json.loads(service_key_json)
+
+# 🔹 Přihlášení do GEE
+credentials = ee.ServiceAccountCredentials(service_account_info['client_email'], service_account_info)
+ee.Initialize(credentials)
+
+#-------------------------------------------------
 # Autentizace a inicializace GEE
 # 🔹 Získání klíče, pokud existuje
-service_key_json = userdata.get("SERVICE_KEY")
+#service_key_json = userdata.get("SERVICE_KEY")
 
 # 🔹 Uživatel musí ručně nastavit svůj klíč
 #if "SERVICE_KEY" not in userdata:
 #    raise ValueError("❌ Nebyl nalezen Service Key! Zadej ho do `userdata['SERVICE_KEY']`.")
 
 # 🔹 Načtení JSON jako slovník
-service_account_info = json.loads(service_key_json)
+#service_account_info = json.loads(service_key_json)
 
 # 🔹 Přihlášení do Google Earth Engine
-credentials = ee.ServiceAccountCredentials(service_account_info['client_email'], service_account_info)
-ee.Initialize(credentials)
-
+#credentials = ee.ServiceAccountCredentials(service_account_info['client_email'], service_account_info)
+#ee.Initialize(credentials)
+#-------------------------------------------------
 #def get_service_key():
 #    """Načte service account key ze Secret Manageru."""
 #    client = secretmanager.SecretManagerServiceClient()
@@ -40,7 +66,7 @@ ee.Initialize(credentials)
 # Přihlášení do Earth Engine
 #credentials = ee.ServiceAccountCredentials(service_key["client_email"], service_key)
 #ee.Initialize(credentials)
-
+#-------------------------------------------------
 # Cesta k JSON klíči
 #key_path = "/content/gee_twi/service-key.json"
 
@@ -48,10 +74,10 @@ ee.Initialize(credentials)
 #service_account = "gee-service-twi@gee-project-twi.iam.gserviceaccount.com"
 #credentials = ee.ServiceAccountCredentials(service_account, key_path)
 #ee.Initialize(credentials)
-
+#-------------------------------------------------
 #ee.Authenticate()
 #ee.Initialize(project = 'gee-project-twi')
-
+#-------------------------------------------------
 # Definice oblasti zájmu (Praha)
 geometry = ee.Geometry.Rectangle([14.2, 50.0, 14.6, 50.2])
 
