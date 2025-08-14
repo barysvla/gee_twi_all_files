6a53a865d552933b1c779ecde3e067ca9b720b59
barysvla <vladislav.baryshev@outlook.com>
2025-08-14 21:04:54 +0200

    Create slope_ee_image.py

---
+++ scripts/slope_ee_image.py
@@ -0,0 +1,11 @@
+import ee
+import geemap
+import numpy as np
+
+def compute_slope(dem,):
+    """
+    Výpočet sklonu terénu na základě DEM.
+    """
+    slope = ee.Terrain.slope(dem).rename("Slope")
+    
+    return slope
