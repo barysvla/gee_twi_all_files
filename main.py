9b179e704073de4f9e9e1c1a77a40d35ce940e44
barysvla <vladislav.baryshev@outlook.com>
2025-09-13 15:01:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import numpy as np
 import tempfile
 import os
 
-from scripts.io_grid import prepare_aligned_dem_and_pixelarea
+from scripts.io_grid import export_dem_and_area_to_arrays
 
 from scripts.fill_depressions import priority_flood_fill
-# from scripts.resolve_flats import resolve_flats_towards_lower_edge
-# from scripts.resolve_flats import resolve_flats_towards_lower_edge_gm
-from scripts.resolve_flats import resolve_flats_barnes
+from scripts.resolve_flats import resolve_flats_barnes_tie
 
 from scripts.flow_direction_quinn_cit import compute_flow_direction_quinn_cit
 from scripts.flow_direction_quinn_1991 import compute_flow_direction_quinn_1991
@@ -18,6 +16,7 @@ from scripts.flow_direction_dz_mfd import compute_flow_direction_dz_mfd
 
 from scripts.flow_accumulation_quinn_cit import compute_flow_accumulation_quinn_cit
 from scripts.flow_accumulation_sfd_inf import compute_flow_accumulation_sfd_inf
+from scripts.flow_accumulation_quinn_1991 import compute_flow_accumulation_quinn_1991
 
 from scripts.push_to_ee import push_array_to_ee_geotiff 
 
@@ -38,60 +37,65 @@ geometry = ee.Geometry.Rectangle([14.2, 50.0, 14.6, 50.2])
 center = geometry.centroid().coordinates().getInfo()
 
 # Načtení DEM
-# FABDEM
-ic = ee.ImageCollection("projects/sat-io/open-datasets/FABDEM")
-# Copernicus GLO-30 ----------------------------
 # 1) Collection
-#ic = ee.ImageCollection('COPERNICUS/DEM/GLO30').select('DEM')  # DSM, band='DEM'  :contentReference[oaicite:1]{index=1}
+# FABDEM 30
+#dem_raw = ee.ImageCollection("projects/sat-io/open-datasets/FABDEM")
 
-# 2) Získej projekci/scale ze „seed“ dlaždice (před mozaikou)
-seed = ic.first()
-proj = seed.projection()                         # má crs + transform
-scale = proj.nominalScale().getInfo()            # ~30 m
+# Copernicus GLO-30 ----------------------------
+dem_raw = ee.ImageCollection('COPERNICUS/DEM/GLO30').select('DEM')  # DSM, band='DEM'  :contentReference[oaicite:1]{index=1}
 
-# 3) Slož mozaiku a nastav jí defaultní projekci seed dlaždice
-dem_raw = (ic.filterBounds(geometry)
-             .mosaic()
-             .setDefaultProjection(proj))         # klíčový krok
-             #.clip(geometry))                    # teď má validní projekci
-# ----------------------------------------------
+# ALOS World 3D 30 m
+#dem_raw = ee.ImageCollection("JAXA/ALOS/AW3D30/V4_1").select('DSM')  # DSM, band='DSM'
+
+# Image
+# 1)
 # SRTM DEM 30m
 #dem_raw = ee.Image('USGS/SRTMGL1_003').select('elevation')
 # SRTM 90m
 #dem_raw = ee.Image('CGIAR/SRTM90_V4').select('elevation')
 # MERIT 90m
 #dem_raw = ee.Image("MERIT/Hydro/v1_0_1").select("elv")
+# ASTER 30m
+#dem_raw = ee.Image("projects/sat-io/open-datasets/ASTER/GDEM").select('b1');
 
-#scale = dem_raw.projection().nominalScale().getInfo()
+grid = export_dem_and_area_to_arrays(
+    src=dem_raw,
+    region_geom=geometry,
+    band=None,
+    resample_method="bilinear", 
+    nodata_value=-9999.0,
+    snap_region_to_grid=True
+)
 
-# Right grid
-grid = prepare_aligned_dem_and_pixelarea(dem_raw, geometry)
-
-dem          = grid["dem"]
+dem_r        = grid["dem"]
 px_area      = grid["pixel_area_m2"]
 transform    = grid["transform"]
 nodata_mask  = grid["nodata_mask"]
+out_crs      = grid["crs"]
 
 # Hydro conditioning
-dem_filled, depth = priority_flood_fill(dem, nodata=np.nan, seed_internal_nodata_as_outlet=True, return_fill_depth=True)
-dem_out, flatmask, labels, stats = resolve_flats_barnes(
-    dem_filled, nodata=np.nan, epsilon=2e-5, equal_tol=0.03, lower_tol=0.0,
-    treat_oob_as_lower=True, require_low_edge_only=True, force_all_flats=False
+dem_filled, depth = priority_flood_fill(dem_r, seed_internal_nodata_as_outlet=True, return_fill_depth=True)
+dem_resolved, flatmask, labels, stats = resolve_flats_barnes_tie(
+    dem_filled, nodata=np.nan, epsilon=2e-5, equal_tol=1e-3, lower_tol=0.0, treat_oob_as_lower=True,
+    require_low_edge_only=True, force_all_flats=False, include_equal_ties=True
 )
 
 # Compute flow direction
-flow_sfd = compute_flow_direction_sfd_inf(dem_out, transform, nodata_mask=nodata_mask)
-
-#flow_dz = compute_flow_direction_dz_mfd(dem_out, p=1.6, nodata_mask=nodata_mask)
+#flow_direction = compute_flow_direction_sfd_inf(dem_resolved, transform, nodata_mask=nodata_mask)
+flow_direction = compute_flow_direction_quinn_1991(dem_resolved, transform, p=1.0, nodata_mask=nodata_mask)
+#flow_direction = compute_flow_direction_dz_mfd(dem_resolved, p=1.6, nodata_mask=nodata_mask)
 
 # Compute flow accumulation
-#acc_km2 = compute_flow_accumulation_sfd_inf(flow_sfd, pixel_area_m2=px_area,
+#acc_km2 = compute_flow_accumulation_sfd_inf(flow_direction, pixel_area_m2=px_area,
 #                                  nodata_mask=nodata_mask, out='km2')
 
-#acc_km2 = compute_flow_accumulation_quinn_cit(flow_dz, pixel_area_m2=px_area, nodata_mask=nodata_mask, out='km2')
+#acc_km2 = compute_flow_accumulation_quinn_cit(flow_direction, pixel_area_m2=px_area, nodata_mask=nodata_mask, out='km2')
 
-acc_cells = compute_flow_accumulation_sfd_inf(flow_sfd, nodata_mask=nodata_mask, out='cells')
-#acc_cells = compute_flow_accumulation_quinn_cit(flow_dz, pixel_area_m2=None, nodata_mask=nodata_mask, out='cells')
+#acc_km2 = compute_flow_accumulation_quinn_1991(flow_direction, pixel_area_m2=px_area, nodata_mask=nodata_mask, out='km2')
+
+#acc_cells = compute_flow_accumulation_sfd_inf(flow_direction, nodata_mask=nodata_mask, out='cells')
+#acc_cells = compute_flow_accumulation_quinn_cit(flow_direction, pixel_area_m2=None, nodata_mask=nodata_mask, out='cells')
+acc_cells = compute_flow_accumulation_quinn_1991(flow_direction, nodata_mask=nodata_mask, out='cells')
 
 # Push numpy array to ee.Image GeoTIFF
 dict_acc = push_array_to_ee_geotiff(
@@ -101,7 +105,7 @@ dict_acc = push_array_to_ee_geotiff(
     nodata_mask=nodata_mask,
     bucket_name=f"{'gee-project-twi'}-ee-uploads",
     project_id="gee-project-twi",
-    band_name="acc_km2",
+    band_name="flow_accumulation",
     tmp_dir=grid.get("tmp_dir", None),
     nodata_value=np.nan,
 )
