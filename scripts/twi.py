b75e22b917f98e51dab78758dd59b35dbf9c81aa
barysvla <vladislav.baryshev@outlook.com>
2025-09-14 23:41:14 +0200

    Update twi.py

--- scripts/twi.py
+++ scripts/twi.py
@@ -7,6 +7,6 @@ def compute_twi(flow_accumulation, slope):
     safe_slope = slope.where(slope.eq(0), 0.1)
     tan_slope = safe_slope.divide(180).multiply(ee.Number(3.14159265359)).tan()
     twi = flow_accumulation.divide(tan_slope).log().rename("TWI")
-    scaled_twi = twi.multiply(1e8).toInt().rename("TWI_scaled")
+    #scaled_twi = twi.multiply(1e8).toInt().rename("TWI_scaled")
 
-    return scaled_twi
+    return twi
